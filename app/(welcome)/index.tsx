943116971a87c25420d5efbbb1faa908919d0772
Ragil Pamungkas <sipamungkas.dev@gmail.com>
2024-07-20 15:30:46 +0700

    feat(welcome): slicing welcome screen

--- app/(welcome)/index.tsx
+++ app/(welcome)/index.tsx
@@ -1,17 +1,43 @@
-import { View, Text } from "react-native";
+import { View, Image } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Button from "@/components/Button";
+import { ThemedText } from "@/components/ThemedText";
+import { Colors } from "@/constants/Colors";
 
 const WelcomeScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{ backgroundColor: Colors.light.white, height: "100%" }}
+    >
       <View>
-        <Text>WelcomeScreen</Text>
+        <ThemedText
+          style={{
+            fontFamily: "RubikRegular",
+            textAlign: "center",
+            marginTop: 24,
+          }}
+          type="title"
+          lightColor={Colors.light.blue}
+        >
+          MiniPOS
+        </ThemedText>
+        <View
+          style={{ alignItems: "center", marginTop: 100, marginBottom: 20 }}
+        >
+          <Image source={require("@/assets/images/onboarding_image_1.png")} />
+        </View>
+        <ThemedText
+          type="subtitle"
+          style={{ textAlign: "center", fontSize: 18, marginBottom: 88 }}
+        >
+          Easy Management for your Store.
+        </ThemedText>
+        <View style={{ paddingHorizontal: 24, gap: 20 }}>
+          <Button title="Login" rounded="lg" />
+          <Button variant="outline" title="Offline Mode" rounded="lg" />
+        </View>
       </View>
-      <Button title="Login" />
-      <Button title="Login" variant="outline" />
-      <Button title="Login" variant="gradient" />
     </SafeAreaView>
   );
 };
