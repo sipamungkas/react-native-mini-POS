c03f36ceea71ff0818fc4cd6d605b8edf1d8c6b8
Ragil Pamungkas <sipamungkas.dev@gmail.com>
2024-07-21 19:56:27 +0700

    feat(auth): add owner login screen

--- app/auth/owner-login/index.tsx
+++ app/auth/owner-login/index.tsx
@@ -1,11 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+import { Colors } from "@/constants/Colors";
+import { ThemedText } from "@/components/ThemedText";
+import { router } from "expo-router";
 
 const OwnerLoginScreen = () => {
   return (
-    <View>
-      <Text>OwnerLoginScreen</Text>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={{ gap: 20, marginBottom: 40 }}>
+        <Input placeholder="ABC2024012222" label="Cashier code" />
+        <Input
+          placeholder="At least 8 characters"
+          label="Password"
+          variant="secure"
+        />
+      </View>
+      <Button title="Login" variant="gradient" rounded="lg" />
+      <ThemedText
+        onPress={() => router.push("/auth/forgot-password")}
+        type="default"
+        style={{
+          textAlign: "center",
+          fontSize: 12,
+          color: Colors.light.blue,
+          textDecorationLine: "underline",
+          fontWeight: "700",
+          marginTop: 12,
+        }}
+      >
+        Forgot Password?
+      </ThemedText>
+    </ScrollView>
   );
 };
 
